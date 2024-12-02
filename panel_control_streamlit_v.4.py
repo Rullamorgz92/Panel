123fbabbc1cdb35ebf677e61eff92d25fadbadbc
Claudioruiz <127865916+Claudioruiz@users.noreply.github.com>
2024-12-02 13:24:24 -0300

    Update panel_control_streamlit_v.4.py

--- panel_control_streamlit_v.4.py
+++ panel_control_streamlit_v.4.py
@@ -1,32 +1,11 @@
 import streamlit as st
+
 st.set_page_config(page_title="Panel de Evaluación", layout="wide")
 
-# Configurar el backend gráfico para Matplotlib
+# Configuración de backend de Matplotlib
 import matplotlib
-matplotlib.use('Agg')  # Backend no interactivo compatible con Streamlit
+matplotlib.use('Agg')  # Configurar un backend compatible con Streamlit
 import matplotlib.pyplot as plt
+
 import pandas as pd
 import numpy as np
-
-@st.cache_data
-def load_data():
-    return pd.read_excel("Informe_Ejecutivosv.10.xlsx")
-
-# Tu código continúa aquí...
-
-
-df = load_data()
-
-# Renombrar columnas para mayor claridad
-df.rename(columns={'Nombre': 'Ejecutivo', 'Contacto': 'Nota Contacto', 'Reunión': 'Nota Reunión'}, inplace=True)
-
-# Título de la aplicación
-st.title("Panel Interactivo de Evaluación de Ejecutivos")
-
-# Sidebar para filtros
-st.sidebar.header("Filtros")
-selected_supervisor = st.sidebar.selectbox("Seleccione un Supervisor", df['Supervisor'].unique())
-filtered_executives = df[df['Supervisor'] == selected_supervisor]['Ejecutivo'].unique()
-selected_executive = st.sidebar.selectbox("Seleccione un Ejecutivo", filtered_executives)
-
-# Código restante...
